651d65627e84cb206e1a79b7b5b983d890a49656
Abhiram B.S.N <abhirambsn@gmail.com>
2022-03-01 11:22:02 +0530

    Initial Commit with API and Client Differentiated

---
+++ client/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col px-3 py-3">
+      <h1 className="text-3xl font-bold text-center underline">Hello World!</h1>
+    </div>
+  )
+}
